5e8f4da5853c85c504492ba2a01700667771b042
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-10 02:30:35 -0400

    Dev: Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -12,13 +12,15 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route paht="/" component={ Login } />
-        <Route paht="/album/:id" component={ Album } />
-        <Route paht="/search" component={ Search } />
-        <Route paht="/favorites" component={ Favorites } />
-        <Route paht="/profile" component={ Profile } />
-        <Route paht="/profile/edit" component={ ProfileEdit } />
-        <Route paht="/*" component={ NotFound } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,78 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+
+const maxName = 3;
 
 class Login extends Component {
+  state = {
+    name: '',
+    isLoading: false,
+    isButtonDisabled: true,
+  };
+
+  handleChangeButton = () => {
+    const { name } = this.state;
+    this.setState({
+      isButtonDisabled: name.length < maxName,
+    });
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    }, this.handleChangeButton);
+  };
+
+  handleClick = async () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+    });
+    history.push('/search');
+  };
+
   render() {
+    const { isButtonDisabled, name, isLoading } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        { isLoading ? <p>Carregando...</p> : (
+          <div>
+            <label htmlFor="input-name">
+              <input
+                data-testid="login-name-input"
+                type="text"
+                name="name"
+                value={ name }
+                id="input-name"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              onClick={ this.handleClick }
+              type="button"
+              disabled={ isButtonDisabled }
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
+
 export default Login;
