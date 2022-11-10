2ca8de6d8e332fe9026b6af0857bb34ff8ae8d76
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-09 22:37:22 -0400

    Dev: Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route paht="/" component={ Login } />
+        <Route paht="/album/:id" component={ Album } />
+        <Route paht="/search" component={ Search } />
+        <Route paht="/favorites" component={ Favorites } />
+        <Route paht="/profile" component={ Profile } />
+        <Route paht="/profile/edit" component={ ProfileEdit } />
+        <Route paht="/*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
