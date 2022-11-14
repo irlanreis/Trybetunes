c75494bf9491161ee291a8ec3c2f791414afdfce
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-14 15:12:30 -0400

    Dev: Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,14 +2,54 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    name: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => this.activeButton());
+  };
+
+  activeButton = () => {
+    const { name } = this.state;
+    const numberLengthMin = 2;
+    const validateButton = name.length < numberLengthMin;
+
+    this.setState({
+      isDisabled: validateButton,
+    });
+  };
+
   render() {
+    const { isDisabled } = this.state;
+
     return (
-      <>
+
+      <form data-testid="page-search">
+        <h1>Search</h1>
         <Header />
-        <div data-testid="page-search">
-          Search
-        </div>
-      </>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="name"
+          onChange={ this.handleChange }
+          placeholder="Nome do Artista"
+        />
+
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ isDisabled }
+          onClick={ ' ' }
+        >
+          Pesquisar
+        </button>
+      </form>
     );
   }
 }
