46db4248d2af64b4d4fecbc0365441cca99c3822
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-15 17:05:31 -0400

    Remove arquivos não utilizados

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../styles/CardMusic.css';
 import { addSong } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
