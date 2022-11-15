735f3a61a1bc55830e6a3c65d38a49e3be3523e3
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-14 22:11:00 -0400

    Dev: Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     name: '',
     isDisabled: true,
+    dataAlbuns: [],
+    isLoading: false,
+    saveArtistBanda: '',
   };
 
   handleChange = ({ target }) => {
@@ -25,30 +31,68 @@ class Search extends Component {
     });
   };
 
+  handleClick = async (e) => {
+    e.preventDefault();
+    const { name } = this.state;
+    this.setState({
+      isLoading: true,
+      saveArtistBanda: name,
+    });
+
+    const containerSongs = await searchAlbumsAPI(name);
+    this.setState({
+      dataAlbuns: containerSongs,
+      isLoading: false,
+      name: '',
+    });
+  };
+
   render() {
-    const { isDisabled } = this.state;
+    const { isDisabled, isLoading, saveArtistBanda, dataAlbuns } = this.state;
 
     return (
 
       <form data-testid="page-search">
         <h1>Search</h1>
         <Header />
-        <input
-          data-testid="search-artist-input"
-          type="text"
-          name="name"
-          onChange={ this.handleChange }
-          placeholder="Nome do Artista"
-        />
+        { isLoading ? <Loading /> : (
+          <>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="name"
+              onChange={ this.handleChange }
+              placeholder="Nome do Artista"
+            />
 
-        <button
-          data-testid="search-artist-button"
-          type="submit"
-          disabled={ isDisabled }
-          onClick={ ' ' }
-        >
-          Pesquisar
-        </button>
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ isDisabled }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+            <div>
+              {`Resultado de álbuns de: ${saveArtistBanda}`}
+            </div>
+            {
+              dataAlbuns.length === 0 ? <div>Nenhum álbum foi encontrado</div> : (
+                dataAlbuns.map((album) => (
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                    key={ album.artistId }
+                  >
+                    <p>{album.collectionName}</p>
+                    <p>{album.artistName}</p>
+                    <img src={ album.artworkUrl100 } alt={ album.collectionId } />
+                  </Link>
+                ))
+              )
+            }
+          </>
+        )}
       </form>
     );
   }
